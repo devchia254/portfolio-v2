903d6975610325da75329388ce33f0142643ce3c
devchia254 <alex7chiaster@gmail.com>
2019-06-27 13:54:47 +0800

    implemented gulp

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
+
+
+// File paths
+const files = { 
+    cssPath: 'assets/css/**/*.css',
+    jsPath: 'assets/js/**/*.js'
+}
+
+// CSS task: adds vendor prefixes and minifies style.css
+function cssTask(){    
+    return src(files.cssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('dist')
+    ); // put final CSS in dist folder
+}
+
+// JS task: concatenates and minifies JS files to script.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(concat('all.js'))
+        .pipe(minify())
+        .pipe(dest('dist')
+    );
+}
+
+// Cachebust
+var cbString = new Date().getTime();
+function cacheBustTask(){
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
+}
+
+// Watch task: watch CSS and JS files for changes
+// If any change, run css and js tasks simultaneously
+function watchTask(){
+    watch([files.cssPath, files.jsPath], 
+        parallel(cssTask, jsTask));    
+}
+
+// Export the default Gulp task so it can be run
+// Runs the css and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+    parallel(cssTask, jsTask), 
+    cacheBustTask,
+    watchTask
+);
