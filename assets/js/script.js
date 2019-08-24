a48b0d7f423b0db17770620f5233d2d36d75b076
devchia254 <alex7chiaster@gmail.com>
2019-08-24 10:27:33 +0800

    removed smooth scroll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //Toggle Menu Animation
-let mainNav = document.getElementById('js-menu');
 let fixedNav = document.getElementById('js-fixed');
+let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
@@ -10,7 +10,7 @@ navBarToggle.addEventListener('click', function () {
 
 //Navbar scroll colour change
 window.onscroll = function changeClass(){
-    var fixedBar = document.getElementById('js-fixed'); 
+    var fixedNav = document.getElementById('js-fixed'); 
     var mainNav = document.getElementById('js-menu');
     var navBar = document.getElementById('js-bar');
 
@@ -18,79 +18,75 @@ window.onscroll = function changeClass(){
     var secondSectionTop = secondSection.getBoundingClientRect().top; //selects the Y position of the element in the view
     var navBarHeight = navBar.getBoundingClientRect().height; //select the height of the navBar
 
-
     if(secondSectionTop <= navBarHeight) {
         navBar.className = ('navbar colour');
         mainNav.className = ('main-nav colour');
-        fixedBar.className = ('fixed-nav colour');
+        fixedNav.className = ('fixed-nav colour');
     } else if(secondSectionTop >= navBarHeight) {
         navBar.className =  ('navbar');
         mainNav.className = ('main-nav');
-        fixedBar.className = ('fixed-nav');
+        fixedNav.className = ('fixed-nav');
     }
-
 }
 
-window.onload = function() {
+// window.onload = function() {
 
-    const easeInCubic = function (t) { return t*t*t }	
-    const scrollElems = document.getElementsByClassName('nav-links');
+//     const easeInCubic = function (t) { return t*t*t }	
+//     const scrollElems = document.getElementsByClassName('nav-links');
     
     
-    //console.log(scrollElems);
-    const scrollToElem = (start, stamp, duration, scrollEndElemTop, startScrollOffset) => {
-        //debugger;
-        const runtime = stamp - start;
-        let progress = runtime / duration;
-        const ease = easeInCubic(progress);
+//     //console.log(scrollElems);
+//     const scrollToElem = (start, stamp, duration, scrollEndElemTop, startScrollOffset) => {
+//         //debugger;
+//         const runtime = stamp - start;
+//         let progress = runtime / duration;
+//         const ease = easeInCubic(progress);
         
-        progress = Math.min(progress, 1);
-        // console.log(startScrollOffset,startScrollOffset + (scrollEndElemTop * ease));
+//         progress = Math.min(progress, 1);
+//         // console.log(startScrollOffset,startScrollOffset + (scrollEndElemTop * ease));
         
-        const newScrollOffset = startScrollOffset + (scrollEndElemTop * ease);
-        window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));
+//         const newScrollOffset = startScrollOffset + (scrollEndElemTop * ease);
+//         window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));
     
-        if(runtime < duration){
-            requestAnimationFrame((timestamp) => {
-                const stamp = new Date().getTime();
-                scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
-            })
-        }
-    }
+//         if(runtime < duration){
+//             requestAnimationFrame((timestamp) => {
+//                 const stamp = new Date().getTime();
+//                 scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+//             })
+//         }
+//     }
     
-    function smoothScroll (e) {
-        e.preventDefault();
-        const scrollElemId = e.target.href.split('#')[1];
-        // alert(scrollElemId);
-        const scrollEndElem = document.getElementById(scrollElemId);
+//     function smoothScroll (e) {
+//         e.preventDefault();
+//         const scrollElemId = e.target.href.split('#')[1];
+//         // alert(scrollElemId);
+//         const scrollEndElem = document.getElementById(scrollElemId);
         
-        const anim = requestAnimationFrame(() => {
-            const stamp = new Date().getTime();
-            const duration = 1200;
-            const start = stamp;
+//         const anim = requestAnimationFrame(() => {
+//             const stamp = new Date().getTime();
+//             const duration = 1200;
+//             const start = stamp;
                 
-            const startScrollOffset = window.pageYOffset;
+//             const startScrollOffset = window.pageYOffset;
         
-            const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top;
+//             const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top;
                     
-            scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
-            // scrollToElem(scrollEndElemTop);
-        })
-    }
+//             scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+//             // scrollToElem(scrollEndElemTop);
+//         })
+//     }
     
-    //Nav Links Scroll Animation
-    for(let i=0; i<scrollElems.length; i++) {
-      const elem = scrollElems[i];  
-        elem.addEventListener('click', smoothScroll);
-    }
+//     //Nav Links Scroll Animation
+//     for(let i=0; i<scrollElems.length; i++) {
+//       const elem = scrollElems[i];  
+//         elem.addEventListener('click', smoothScroll);
+//     }
 
-    //Show More button Scroll Animation
-    const button = document.getElementById("know-more");
+//     //Show More button Scroll Animation
+//     const button = document.getElementById("know-more");
 
-    button.addEventListener('click', smoothScroll);
+//     button.addEventListener('click', smoothScroll);
 
-    console.log(button);
-
-
-}
+//     console.log(button);
+// }
 
